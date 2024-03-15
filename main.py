83957cd083365bcf447fec2f77863625125e2998
s28370 <s28370@pjwstk.edu.pl>
2024-03-15 17:14:03 +0100

    Task 2 Used function to calculate square

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 # 1
-squares = [x**2 for x in range(1, 11)]
-print(squares)
+# squares = [x**2 for x in range(1, 11)]
+# print(squares)
+
+# 2
+def e_squares(start, end):
+    squares = [x**2 for x in range(start, end + 1)]
+    return squares
+
+
+print(e_squares(5, 12))
