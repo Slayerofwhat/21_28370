2ac2d5b19583d1aa190c38dee94cf171a197bf94
Maksym <makcim2004leb@gmail.com>
2024-03-17 19:57:05 +0100

    Task 3 created class to find squares

--- main.py
+++ main.py
@@ -3,9 +3,26 @@
 # print(squares)
 
 # 2
-def e_squares(start, end):
-    squares = [x**2 for x in range(start, end + 1)]
-    return squares
+# def e_squares(start, end):
+#     squares = [x**2 for x in range(start, end + 1)]
+#     return squares
+#
+#
+# print(e_squares(5, 12))
+
+# 3
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        squares = []
+        for num in range(start, end + 1):
+            squares.append(num ** 2)
+        return squares
 
 
-print(e_squares(5, 12))
+start_num = 2
+end_num = 15
+
+square_gen = SquareGenerator()
+
+squares_list = square_gen.generate_squares(start_num, end_num)
+print("From", start_num, "to", end_num, ":", squares_list)
