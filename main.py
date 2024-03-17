52b87d595fb84d4f3f1f9067e2b5c07bba6a8e44
Maksym <makcim2004leb@gmail.com>
2024-03-17 22:51:56 +0100

    Task 5 handled case when start is greater than end

--- main.py
+++ main.py
@@ -12,8 +12,33 @@ import math
 # print(e_squares(5, 12))
 
 # 3
+# class SquareGenerator:
+#     def generate_squares(self, start, end):
+#         squares = []
+#         for num in range(start, end + 1):
+#             squares.append(num ** 2)
+#         return squares
+#
+#
+# start_num = 2
+# end_num = 15
+#
+# square_gen = SquareGenerator()
+#
+# squares_list = square_gen.generate_squares(start_num, end_num)
+# print("From", start_num, "to", end_num, ":", squares_list)
+#
+#
+# 4
+# square_roots = [math.sqrt(num) for num in squares_list]
+# print("Squares Roots:", square_roots)
+
+# 5
 class SquareGenerator:
     def generate_squares(self, start, end):
+        if start > end:
+            raise Exception("start is greater than end")
+
         squares = []
         for num in range(start, end + 1):
             squares.append(num ** 2)
@@ -21,14 +46,9 @@ class SquareGenerator:
 
 
 start_num = 2
-end_num = 15
+end_num = 1
 
 square_gen = SquareGenerator()
 
 squares_list = square_gen.generate_squares(start_num, end_num)
 print("From", start_num, "to", end_num, ":", squares_list)
-
-
-# 4
-square_roots = [math.sqrt(num) for num in squares_list]
-print("Squares Roots:", square_roots)
